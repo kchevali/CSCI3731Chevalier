946957c43c53d9fbcb44ef31a799d7c42795ddbb
Kevin Chevalier <kevin.chevalier10@gmail.com>
2019-08-28 18:20:39 -1000

    Changed Comments + Added Prompts
    HW1

--- hw01/bitbybit.cpp
+++ hw01/bitbybit.cpp
@@ -19,36 +19,45 @@ g++ bitbybit.cpp maxmin.cpp -o bitbybit.exe && ./bitbybit.exe
 
 int main(int argc, char** argv) {
   int n, res;
-  res = scanf("%d\n", &n);  // number of instr
-  while (n != 0) {          // exit if 0
-    int i;                  // index with for
-    int pos1, pos2;         // bit index for instr
-    int bits[32];           // all bits //code: 0=0,?=1,1=2
-    fpp(32) bits[i] = 1;    // all set: ?
+  printf("Valid Instructions: (CLEAR, SET, OR, AND)\n");
+
+  printf("Number of Instructions:");
+  res = scanf("%d", &n);  // number of instructions
+  while (n != 0) {        // exit if 0
+    int i;                // index - for
+    int pos1, pos2;       // bit index for instr
+    int bits[32];         // 32 bit reg. //code: 0=0,?=1,1=2
+    fpp(32) bits[i] = 1;  // set all: ?
     fpp(n) {
-      char str[5];                       // instr name
-      res = scanf("%s %d", str, &pos1);  // name index1
+      char str[5];  // instr name
+      printf("Instruction:");
+      res = scanf("%s", str);  // instr name
+      printf("First Index:");
+      res = scanf("%d", &pos1);  // reg index1
       char ch = str[0];
 
-      if (ch == 'C')  // Clear
+      if (ch == 'C')  // Clear instr
         bits[pos1] = 0;
-      else if (ch == 'S')  // Set
+      else if (ch == 'S')  // Set instr
         bits[pos1] = 2;
       else {
-        res = scanf("%d", &pos2);  // index2
+        printf("Second Index:");
+        res = scanf("%d", &pos2);  // reg index2
 
-        if (ch == 'O')  // OR
+        if (ch == 'O')  // Or instr
           max(bits[pos1], bits[pos2]);
-        else  // AND
+        else  // And instr
           min(bits[pos1], bits[pos2]);
       }
     }
 
     // OUTPUT
+    printf("Register: ");
     fmm(32) { printf("%c", bits[i] != 1 ? (bits[i] == 2 ? '1' : '0') : '?'); }
     printf("\n");
 
     // RESTART
+    printf("\nNumber of Instructions:");
     res = scanf("%d", &n);
   }
   // DONE
