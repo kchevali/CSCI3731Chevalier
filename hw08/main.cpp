c970de7382f7c4564b254fe5b367dd13655762c0
Kevin Chevalier <kevin.chevalier10@gmail.com>
2019-10-31 12:51:17 -1000

    HW8:Short Answers + Minor
    Increased image size

--- hw08/main.cpp
+++ hw08/main.cpp
@@ -20,22 +20,21 @@ int main(int argc, char** argv) {
   img.writeJPEG("output.jpeg", 100);
 
   // The Good Stuff
-  Image canvas = Image(360, 360);
+  Image canvas = Image(1024, 1024);
   // canvas.fill(51);
 
-  //determine if sequence diverges
+  // determine if sequence diverges
   const int max = 1000;
 
-  //all pixels
+  // all pixels
   for (int i = 0; i < canvas.getHeight(); i++) {
     for (int j = 0; j < canvas.getWidth(); j++) {
-
-      //map coord to zoom
+      // map coord to zoom
       double a = map(j, 0, canvas.getWidth(), -1.5, 1.5);
       double b = map(i, 0, canvas.getHeight(), -1.5, 1.5);
       double x = a, y = b;
 
-      //mandrelbrot algorithm
+      // mandrelbrot algorithm
       int n, z = 0;
       for (n = 0; n < max; n++) {
         double aa = a * a - b * b;
@@ -48,21 +47,21 @@ int main(int argc, char** argv) {
         }
       }
 
-      //get brightness
+      // get brightness
       double brightness = map(n, 0, max, 0, 1);
       brightness = n == max ? 0 : map(sqrt(brightness), 0, 1, 0, 255);
 
-      //color the canvas
+      // color the canvas
       canvas.setPixel(i, j, brightness, brightness, brightness);
     }
   }
 
-  //save image
+  // save image
   canvas.writeJPEG("mandelbrot.jpeg", 100);
   std::cout << "Done" << std::endl;
 }
 
-//maps value from [a, b] to [x,y]
+// maps value from [a, b] to [x,y]
 double map(double value, double a, double b, double x, double y) {
   return (value - a) * (y - x) / (b - a) + x;
 }
